098cdb309e16eb9c72195ecfb3c437f643b5e49b
Kevo <me@kevo.link>
2021-10-28 19:26:38 +0200

    __main__.py

--- mst/__main__.py
+++ mst/__main__.py
@@ -6,3 +6,13 @@
             3.2. `save.py`
 """
 
+import asyncio
+
+from mst.save import scrap_from_all_and_save
+
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(scrap_from_all_and_save())
+    loop.close()
